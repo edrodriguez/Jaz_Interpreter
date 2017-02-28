6c0e106d3bb56ba11eead7efdcec98da6be97758
ed.roma19 <ed.roma19@gmail.com>
2017-02-28 17:44:38 -0500

    Get rid of test input parameter

--- JazInterpreter/JazInterpreter/Main.cpp
+++ JazInterpreter/JazInterpreter/Main.cpp
@@ -20,68 +20,16 @@ int main(int argc, char* argv[])
 	//Check for input parameters
 	if (argv[1] != nullptr)
 	{
-		string mode = argv[1];
+		string file = argv[1];	
+		vector<string> instructions;
 
-		//Run the appropriate mode of Operation
-		if (mode == "-test")
-		{
-			cout << "Running Tests for Intepreter" << endl;
-			//Tests
-			return 0;
-		}
-		else if (mode == "-interpret")
-		{
-			//string inputFileName, outputFileName;
-			string file;
-
-			if (argv[2] != nullptr)
-				file = argv[2];
-			else
-			{
-				cout << "Error running the interpreter. You need to provide a source file." << endl;
-				return 0;
-			}
-
-			////Get filenames. Input file required, output optional
-			//if (file == "-infile")
-			//{
-			//	if (argv[3] != nullptr)
-			//		inputFileName = argv[3];
-
-			//	if (argv[4] != nullptr && argv[4] == "-outfile")
-			//		outputFileName = argv[5];
-			//	else
-			//		outputFileName = "Results.txt";
-			//}
-			//else if (argv[2] == "-outfile")
-			//{
-			//	if (argv[3] != nullptr)
-			//		outputFileName = argv[3];
-
-			//	if (argv[4] != nullptr && argv[4] == "-infile")
-			//		outputFileName = argv[5];
-			//	else
-			//	{
-			//		cout << "Error running the interpreter. You need to provide a source file." << endl;
-			//	}
-			//}
-			//else
-			//{
-			//	cout << "Error running the interpreter. You need to provide a source file." << endl;
-			//	return 0;
-			//}
-
-			//Intepreter Operation
-			vector<string> instructions;
-			ReadFile(file, instructions);
-			separateInstructions(instructions);
-			extractLabels();
-			runProgram();
-			WriteFile(file);
-		}
+		ReadFile(file, instructions);
+		separateInstructions(instructions);
+		extractLabels();
+		runProgram();
+		WriteFile(file);
 	}
 	else
-		cout << "Error running the interpreter. You need to provide mode of operation." << endl;
-
+		cout << "Error running the interpreter. You need to provide a source file." << endl;
 	return 0;
 }
